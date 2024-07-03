d885eee23cb4a5db8bc3524d51a46a8a1d6a5b17
mshakhriyorov <mshakhriyorov8@gmail.com>
2024-07-03 18:51:53 +0500

    +

--- src/components/ui/search-input.jsx
+++ src/components/ui/search-input.jsx
@@ -2,7 +2,7 @@
 
 import { useCallback, useMemo, useRef } from 'react'
 import { debounce } from 'lodash'
-import { useSetAtom } from 'jotai'
+import { useAtom } from 'jotai'
 import { MicroscopeIcon, XIcon } from 'lucide-react'
 
 import { Input } from '@/components/ui/input'
@@ -11,13 +11,13 @@ import { inputKeyword } from '@/lib/atoms/filters-atom'
 
 export function SearchInput() {
   const inputRef = useRef(null)
-  const setInputValue = useSetAtom(inputKeyword)
+  const [inputValue, setInputValue] = useAtom(inputKeyword)
 
   const debouncedChangeHandler = useMemo(
     () =>
       debounce((value) => {
         setInputValue(value)
-        window.scrollTo({ top: 100, behavior: 'smooth' })
+        window.scrollTo({ top: 70, behavior: 'smooth' })
       }, 500),
     [setInputValue]
   )
@@ -42,7 +42,7 @@ export function SearchInput() {
         className="w-full min-w-96 py-3 pl-4 pr-10"
         onChange={handleChange}
       />
-      {inputRef.current?.value?.length > 0 ? (
+      {inputValue?.length > 0 ? (
         <XIcon
           className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2 transform cursor-pointer"
           onClick={clearInput}
